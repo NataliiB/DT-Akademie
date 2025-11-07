bdd8b81389e107ad236204ab79e763dd4427bae0
NataliiB <nataliibondarenko@ukr.net>
2025-11-07 13:04:13 +0100

    update task1

--- task1.py
+++ task1.py
@@ -1,19 +1,26 @@
 # Сумма двох чисел: Написати програму, яка зчитує два цілих числа і виводить
 
-# num1 = int(input("Please enter number1: "))
-# num2 = int(input("Please enter number2: "))
-# print(num1 + num2)
+num1 = int(input("Please enter number1: "))
+num2 = int(input("Please enter number2: "))
+print(num1 + num2)
 
 # Гіпотенуза за двома катетами прямокутного трикутника знайти довжину його гіпотенузи 
+
 cat1 = int(input("Please enter number1: "))
 cat2 = int(input("Please enter number2: "))
 print("Hypotenuse = ",(cat1 ** 2 + cat2 ** 2) ** 0.5)
+
 # Двічі: написати програму яка зчитує ціле число то виводить його двічі
+
 num = int(input("Please enter number: "))
 print(str(num) * 2)
+
 #  Привіт: написати програму яка запитує ім'я користувача і виводить привітання
+
 name = input("Please enter your name: ")
 print("Your name is",name)
+
 #  Прощавай: написати програму яка запитує ім'я користувача і виводить прощання
+
 name = input("Please enter your name: ")
 print("Buy, " + name + "!")
