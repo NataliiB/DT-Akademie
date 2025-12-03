608767a2c88418896b5525d8a87c32d5e9d119dc
NataliiB <nataliibondarenko@ukr.net>
2025-12-03 21:52:24 +0100

    Update list and tuple tasks

--- task_list_tuple.py
+++ task_list_tuple.py
@@ -1,33 +1,28 @@
 # 1. Створіть список на основі введеної послідовності цілих чисел і надрукуйте другу половину списку. Якщо кількість не парна, то вивести більшу кількість.
 
 s = input("Please enter a consistency of numbers: ")
-list_of_s = [i for i in s if i.isdigit()]
-
-for i in range(len(list_of_s)):
-    def print_num(i):
-         if i >= len(list_of_s):
-             return
-         elif i >= round(len(list_of_s)/2):
-            print (list_of_s[i])
-    print_num(i)
+def print_half(s):
+    list_from_s = s.strip().split()
+    if len(list_from_s) % 2 == 1:
+        print(list_from_s[(round(len(list_from_s)/2)-1):])
+    else:
+        print(list_from_s[(round(len(list_from_s)/2)):])  
+print_half(s)
 
 # 2. Створіть список на основі введеної послідовності цілих чисел і надрукуйте його елементи таким чином: два останні елементи переміщені з кінця в початок списку без зміни їх початкового порядку.
 
 s = input("Please enter a consistency of numbers: ")
-list_of_s = [i for i in s if i.isdigit()]
-new_list = (list_of_s[len(list_of_s) - 2:len(list_of_s)]) + ( list_of_s[0:len(list_of_s) - 2])
+list_from_s = s.strip().split()
+new_list = (list_from_s[len(list_from_s) - 2:len(list_from_s)]) + ( list_from_s[0:len(list_from_s) - 2])
+print(new_list)
 
 # 3. Збережіть назви мов світу, які вводяться в одному рядку через пропуск, у списку. Простежте за тим, щоб елементи у списку не зберігались в алфавітному порядку. Відсортуйте список в алфавітному порядку і виведіть його елементи в рядку через пропуск.
 
 s = input("Please enter languages: ")
-list_of_s = s.strip().split()
-is_sorted = 0
-for i in range(len(list_of_s)-1):
-  if list_of_s[i] <= list_of_s[i + 1]:
-    is_sorted += 1
-    if is_sorted < len(list_of_s)-1:
-     list_of_s.sort()
-print(" ".join(list_of_s))
+def print_sorted(s):
+    list_from_s = s.strip().split()
+    print(' '.join(sorted(list_from_s)))
+print_sorted(s)
 
 # 4. Виведіть елементи даного списку в зворотному порядку, не змінюючи сам список.
 
